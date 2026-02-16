178b36ee826e8542368e8477d7a42d7fcb1e7c06
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-16 20:10:42 +0530

    learn basic like data type comments and the veriable

---
+++ 1.basic (veriable comments etc..)/1_comments and veriable.py
@@ -0,0 +1,20 @@
+# hash tag is used write comments in the python 
+# comments are the words like.. ignored by the compiler this is used to just expalin the code
+"""this is the doc string comment"""
+# veriable are used to store words,number... syntax-- veriableName=the value to write
+# rules--
+# not starts form number like(23vasf)
+# not leave black space like(varun thakur) do (varun_thakur)
+#not used any special character only ('_' is allowed )
+
+# nameing convention
+# cammenl case--varunThakur
+# pascal case --VarunThakur
+# snake case --varun_thakur
+# data type 
+a=343
+print(type(a)) #this is the int data type
+b=34.34
+print(type(b))#this is float
+c=12/3
+print(type(c))#this is also float
