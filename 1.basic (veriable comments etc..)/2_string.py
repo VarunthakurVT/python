57776d8fefafdee7fb722145bf1f8f000ff276b3
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-17 20:18:47 +0530

    understand the cocept of the string and some type conversion

---
+++ 1.basic (veriable comments etc..)/2_string.py
@@ -0,0 +1,12 @@
+# there is a unique code for the character..
+a="A"
+print(ord(a))
+print(chr(65))
+#indexing in the string---
+#starts form 0 and end with n-1
+# there is - ve indexing starts form -1 starts form last 
+# string access by []
+name="varun thakur"
+print(name[0])
+# there is a concept called string slicing syntax variable name[start:stop(excluded):stepsize]
+print(name[0:5:1])
---
+++ 1.basic (veriable comments etc..)/3_type conversion.py
@@ -0,0 +1,10 @@
+# there is type conversion to change the data type of the veriable
+a="10"
+print(type(a))
+a=int(a)
+print(type(a))
+a=0
+print(bool(a))
+#there are two types of type conversion 
+#1)inplicit 
+#2)explicit
