ac46e172606573c49c476b42dcd7ec804f3b8770
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-19 20:16:25 +0530

    learning conditional statements

---
+++ 4.conditional statements/1_if else.py
@@ -0,0 +1,11 @@
+# if condition:
+#5spaces do the work if the condition is true 
+# else:
+    #  if the if condition is false
+
+#we do this with simple programe like you are adult or not
+age=int(input("Enter your age "))
+if age>=18:
+    print("you are adult now")
+else:
+    print("you are not adult")
---
+++ 4.conditional statements/2_if elif else.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age "))
+if age>=18:
+    print("you are adult now")
+elif age<=13:
+    print("you are teenager")
+else:
+    print("you are not adult")
